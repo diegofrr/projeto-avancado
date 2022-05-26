997ae5528d6fb83e800cfcced07032f02d9857ff
Diêgo Ferreira <56923620+diegofrr@users.noreply.github.com>
2022-05-26 17:51:58 -0300

    Add files via upload

---
+++ exerc-react-hooks/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ListaTarefas from './components/ListaTarefas';
+import './App.css'
+
+function App() {
+  
+  return(
+    <div>
+      <ListaTarefas/>
+    </div>
+  )
+
+}
+
+export default App;
